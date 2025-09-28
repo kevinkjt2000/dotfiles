753c84dca7348a84ab9a776dfc4c9a09b5acf137
Kevin Tindall <kevinkjt2000@gmail.com>
2025-09-28 11:58:07 +0200

    Improve nvim's display of warnings and errors

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -14,3 +14,17 @@ vim.keymap.set("n", "<leader><leader>", "<C-^>")
 
 -- TODO Change CtrlP to open with <leader>f instead
 -- TODO Change CtrlP buffer mode to open with <leader>b
+
+-- Improve display of warnings and errors
+vim.diagnostic.config {
+  severity_sort = true,
+  float = { border = 'rounded', source = 'if_many' },
+  virtual_text = {
+    current_line = true,
+    source = 'if_many',
+    spacing = 2,
+    format = function(diagnostic)
+      return diagnostic.message
+    end,
+  },
+}
